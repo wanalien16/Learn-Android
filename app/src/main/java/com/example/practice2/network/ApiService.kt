412e8dd3ad6daf22f6e2aac1019f0f303618bdbd
anilsai <anilsai.damayapally16@gmail.com>
2024-09-06 19:31:38 +0530

    added Repositories data class and fetched list of trending repositories from the api in the MainScreen.

--- app/src/main/java/com/example/practice2/network/ApiService.kt
+++ app/src/main/java/com/example/practice2/network/ApiService.kt
@@ -1,10 +1,14 @@
 package com.example.practice2.network
 
 import com.example.practice2.data.Developers
+import com.example.practice2.data.Repositories
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface ApiService {
     @GET("developers")
    suspend fun getDevelopers(@Query("language")language: String): List<Developers>
+
+   @GET("repositories")
+   suspend fun getRepositories(@Query("language") language: String): List<Repositories>
 }
--- app/src/main/java/com/example/practice2/screens/MainScreen.kt
+++ app/src/main/java/com/example/practice2/screens/MainScreen.kt
@@ -4,8 +4,12 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
@@ -20,39 +24,76 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
 import com.example.practice2.data.Developers
+import com.example.practice2.data.Repositories
 
 @Composable
 
 fun Mainscreen(viewModel: MainScreenViewModel = MainScreenViewModel()){
-    val developers by viewModel.state.collectAsState()
+    val developers by viewModel.developerstate.collectAsState()
+    val repositories by viewModel.repositoriesstate.collectAsState()
+Column {
+    Spacer(modifier = Modifier.padding(30.dp))
+    LazyColumn(modifier = Modifier.wrapContentHeight()) {
+        items(developers){
+                developer -> cardView(developer)
 
-   LazyColumn(modifier = Modifier.fillMaxSize()) {
-       items(developers){
-           developer -> cardView(developer)
 
-       }
-   }
-    
-    
+        }
+    }
+    Spacer(modifier = Modifier.padding(30.dp))
+    LazyColumn(modifier = Modifier.wrapContentHeight()) {
+        items(repositories){
+                repository -> cardViewofRepository(repository)
+
+
+        }
+    }
+}
+
 
 }
 @Composable
 fun cardView(developer: Developers){
-    Card {
+    Card(modifier = Modifier.fillMaxWidth()) {
         Row {
             Image(painter = rememberAsyncImagePainter(developer.avatar),
                 contentDescription = "Developer Image",
                         modifier = Modifier
                             .size(60.dp)
                             .clip(CircleShape)
+                            .padding(5.dp)
                             .background(
                                 MaterialTheme.colorScheme.surface
                             ))
+            Spacer(modifier = Modifier.padding(5.dp))
             
-            Column {
+            Column(modifier = Modifier.padding(5.dp)) {
                 Text(text = developer.username)
                 
                 Text(text = developer.url)
+            }
+        }
+    }
+}
+@Composable
+fun cardViewofRepository(repository: Repositories){
+    Card(modifier = Modifier.fillMaxWidth()) {
+        Row {
+            Image(painter = rememberAsyncImagePainter(repository.avatar),
+                contentDescription = "Developer Image",
+                modifier = Modifier
+                    .size(60.dp)
+                    .clip(CircleShape)
+                    .padding(5.dp)
+                    .background(
+                        MaterialTheme.colorScheme.surface
+                    ))
+            Spacer(modifier = Modifier.padding(5.dp))
+
+            Column(modifier = Modifier.padding(5.dp)) {
+                Text(text = repository.name)
+
+                Text(text = repository.url)
             }
         }
     }
